3d7a7b38f89365a322e422fe145d91ee3b8ca6ba
Vincent Demeester <vincent@sbr.pm>
2016-08-22 23:22:19 +0200

    Remove mouse max/time customization
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- config.h
+++ config.h
@@ -3,12 +3,6 @@
 
 #include "../../config.h"
 
-#undef MOUSEKEY_TIME_TO_MAX
-#undef MOUSEKEY_MAX_SPEED
-
-#define MOUSEKEY_TIME_TO_MAX 1
-#define MOUSEKEY_MAX_SPEED 2
-
 #undef MOUSEKEY_DELAY
 #define MOUSEKEY_DELAY 0
 
